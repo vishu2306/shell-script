2658978d09916392e2f93b0ee43aa374c6dca3f6
vishu2306 <viswanadha.kalluri@gmail.com>
2024-01-11 20:04:31 +0530

    Credentials Added

---
+++ 05-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Please enter your Username::"
+read -s USERNAME
+echo "Username is:$USERNAME"
+echo "Please enter your Password::"
+read -s PASSWORD
+echo "Password is:$PASSWORD"
